f55fd9a7c0d5edbf241d3180937dcf7da4f7f452
Alvaro Gabriel <alvarogabriel@outlook.com.ar>
2023-06-15 20:51:11 -0300

    loginuser, loginloader, logout, ruta protegida, status 401-404 y CSRF-token

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, redirect, render_template, request, url_for, flash
 from flask_mysqldb import MySQL
+from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager, login_required, login_user, logout_user
 
 from config import config
 
@@ -11,7 +13,13 @@ from models.entities.User import User
 
 app = Flask(__name__)
 
+csrf = CSRFProtect()
 db = MySQL(app)
+login_manager_app = LoginManager(app)
+
+@login_manager_app.user_loader
+def load_user(id):
+    return ModelUser.get_by_id(db, id)
 
 @app.route('/')
 def index():
@@ -26,6 +34,7 @@ def login():
         logged_user = ModelUser.login(db, user)
         if logged_user != None:
             if logged_user.password:
+                login_user(logged_user)
                 return redirect(url_for('home'))
             else:
                 flash("Invalid password...")
@@ -36,10 +45,29 @@ def login():
     else:
         return render_template('auth/login.html')
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 @app.route('/home')
 def home():
     return render_template('home.html')
 
+@app.route('/protected')
+@login_required
+def protected():
+    return "<h1>Esta es una vista protegida, solo para usuarios autenticados.</h1>"
+
+def status_401(error):
+    return redirect(url_for('login'))
+
+def status_404(error):
+    return "<h1>Página no encontrada</h1>",404
+
 if __name__ == '__main__':
     app.config.from_object(config['development'])
+    csrf.init_app(app)
+    app.register_error_handler(401,status_401)
+    app.register_error_handler(404, status_404)
     app.run()
--- src/models/ModelUser.py
+++ src/models/ModelUser.py
@@ -7,12 +7,26 @@ class ModelUser():
         try:
             cursor = db.connection.cursor()
             sql = """SELECT id, username, password, fullname FROM user
-                    WHERE username = '{}'""".format(user.username)
+                 WHERE username = '{}'""".format(user.username)
             cursor.execute(sql)
             row = cursor.fetchone()
             if row != None:
                 user = User(row[0], row[1], User.check_password(row[2], user.password), row[3])
                 return user
+            else:
+                return None
+        except Exception as ex:
+            raise Exception(ex)
+        
+    @classmethod
+    def get_by_id(self, db, id):
+        try:
+            cursor = db.connection.cursor()
+            sql = "SELECT id, username, fullname FROM user WHERE id = {}".format(id)
+            cursor.execute(sql)
+            row = cursor.fetchone()
+            if row != None:
+                return User(row[0], row[1], None, row[2])   
             else:
                 return None
         except Exception as ex:
